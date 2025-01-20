626912b99b950851df069fd4a218042babdff287
Abednehme-coder <ahn314@student.bau.edu.lb>
2025-01-20 23:27:10 +0200

    Delete oop currency converter project directory

--- oop currency converter project/src/guiLogin/Currency.java
+++
@@ -1,7 +0,0 @@
-package guiLogin;
-
-public abstract class Currency {
-	protected abstract double to_Dollar();
-	protected abstract double to_LL();
-	protected abstract double to_Euro();
-}
--- oop currency converter project/src/guiLogin/Ores.java
+++
@@ -1,7 +0,0 @@
-package guiLogin;
-
-public abstract class Ores {
-	protected abstract double to_Dollar();
-	protected abstract double to_Ore();
-	
-}
